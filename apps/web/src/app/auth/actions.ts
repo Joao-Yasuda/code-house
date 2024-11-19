a2ef77ec9fd59ba5b3373746a256b5d5aa5c5016
Joao <joaoyasuda@gmail.com>
2024-11-19 12:55:53 -0800

    light them and env

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,9 +1,13 @@
 'use server'
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 export async function signInWithGithub() {
   const githubSignInURL = new URL('login/oauth/authorize', 'https://github.com')
-  githubSignInURL.searchParams.set('client_id', 'Ov23lipCP1hcYem3fgnz')
-  githubSignInURL.searchParams.set('redirect_uri', 'http://localhost:3000/api/auth/callback')
+  githubSignInURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
+  githubSignInURL.searchParams.set(
+    'redirect_uri',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
+  )
   githubSignInURL.searchParams.set('scope', 'user')
   redirect(githubSignInURL.toString())
 }
---
+++ apps/web/src/components/header.tsx
@@ -0,0 +1,38 @@
+import { Slash } from 'lucide-react'
+import Image from 'next/image'
+import { ThemeSwitcher } from './theme/theme-switcher'
+import { Separator } from './ui/separator'
+
+import logoIcon from '@/assets/logo-icon.svg'
+import { ability } from '@/auth/auth'
+
+import { OrganizationSwitcher } from './organization-switcher'
+import { ProfileButton } from './profile-button'
+
+export async function Header() {
+  const permissions = await ability()
+
+  return (
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image
+          src={logoIcon}
+          className="size-6 dark:invert"
+          alt="logo"
+        />
+
+        <Slash className="size-3 -rotate-[24deg] text-border" />
+
+        <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>Project</p>}
+      </div>
+
+      <div className="flex items-center gap-4">
+      <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
+        <ProfileButton />
+      </div>
+    </div>
+  )
+}
