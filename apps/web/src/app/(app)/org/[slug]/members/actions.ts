476656de99dd316fcd64671413df23594f653f1f
Joao <joaoyasuda@gmail.com>
2024-11-30 13:03:35 -0800

    invite actions

---
+++ apps/web/src/app/(app)/org/[slug]/members/actions.ts
@@ -0,0 +1,40 @@
+'use server'
+
+import { getCurrentOrg } from '@/auth/auth'
+import { removeMember } from '@/http/remove-member'
+import { revokeInvite } from '@/http/revoke-invite'
+import { updateMember } from '@/http/update-member'
+import { Role } from '@saas/auth'
+import { revalidateTag } from 'next/cache'
+
+export async function removeMemberAction(memberId: string) {
+  const currentOrg = getCurrentOrg()
+
+  await removeMember({
+    org: currentOrg!,
+    memberId,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+
+export async function updateMemberAction(memberId: string, role: Role) {
+  const currentOrg = getCurrentOrg()
+
+  await updateMember({
+    org: currentOrg!,
+    memberId,
+    role,
+  })
+
+  revalidateTag(`${currentOrg}/members`)
+}
+
+export async function revokeInviteAction(inviteId: string) {
+  const currentOrg = getCurrentOrg()
+  await revokeInvite({
+    org: currentOrg!,
+    inviteId,
+  })
+  revalidateTag(`${currentOrg}/invites`)
+}
--- apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/member-list.tsx
@@ -1,3 +1,7 @@
+import { organizationSchema } from '@saas/auth'
+import { ArrowLeftRight, Crown, UserMinus } from 'lucide-react'
+import Image from 'next/image'
+
 import { ability, getCurrentOrg } from '@/auth/auth'
 import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
@@ -5,21 +9,26 @@ import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
 import { getMembers } from '@/http/get-members'
 import { getMembership } from '@/http/get-membership'
 import { getOrganization } from '@/http/get-organization'
-import { organizationSchema } from '@saas/auth'
-import { ArrowLeftRight, Crown } from 'lucide-react'
-import Image from 'next/image'
+
+import { removeMemberAction } from './actions'
+import { UpdateMemberRoleSelect } from './update-member-role-select'
+
 export async function MemberList() {
   const currentOrg = getCurrentOrg()
   const permissions = await ability()
+
   const [{ membership }, { members }, { organization }] = await Promise.all([
     getMembership(currentOrg!),
     getMembers(currentOrg!),
     getOrganization(currentOrg!),
   ])
+
   const authOrganization = organizationSchema.parse(organization)
+
   return (
     <div className="space-y-2">
       <h2 className="text-lg font-semibold">Members</h2>
+
       <div className="rounded border">
         <Table>
           <TableBody>
@@ -67,6 +76,33 @@ export async function MemberList() {
                           <ArrowLeftRight className="mr-2 size-4" />
                           Transfer ownership
                         </Button>
+                      )}
+
+                      <UpdateMemberRoleSelect
+                        memberId={member.id}
+                        value={member.role}
+                        disabled={
+                          member.userId === membership.userId ||
+                          member.userId === organization.ownerId ||
+                          permissions?.cannot('update', 'User')
+                        }
+                      />
+
+                      {permissions?.can('delete', 'User') && (
+                        <form action={removeMemberAction.bind(null, member.id)}>
+                          <Button
+                            disabled={
+                              member.userId === membership.userId ||
+                              member.userId === organization.ownerId
+                            }
+                            type="submit"
+                            size="sm"
+                            variant="destructive"
+                          >
+                            <UserMinus className="mr-2 size-4" />
+                            Remove
+                          </Button>
+                        </form>
                       )}
                     </div>
                   </TableCell>
